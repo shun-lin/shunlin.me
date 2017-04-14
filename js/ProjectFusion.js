964d1083be5aa299af85f897798b5140f8c89f74
Shun Lin <s92014721@berkeley.edu>
2017-04-14 14:17:17 -0700

    mimize some images

--- js/ProjectFusion.js
+++ js/ProjectFusion.js
@@ -5,6 +5,26 @@ $(document).ready(function() {
       var location = document.getElementById("Location").value;
       Materialize.toast(location, 2000);
 
-      $.get("https://api.yelp.com/v3/businesses/search?term=food&location=california");
+      $.all('/*', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        next();
+      });
+
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.yelp.com/v3/businesses/search?term=delis&location=california",
+        "method": "GET",
+        "headers": {
+          "authorization": "Bearer _V_7p2G5Uo_dfJLHJ50dXc7-xlIxSf8_fjn-jyegCmVHfSSu2MMdyVBGmd235kSkpvV0gu753vPVA0K7sftwbnKy6Kl95DRaCWF2EXOcRcpnri1c-H0vNOsVUdzdWHYx",
+          "cache-control": "no-cache",
+          "postman-token": "31c1e590-1259-94e0-5c58-631a29a2d6e5"
+        }
+      }
+
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
     });
 });
