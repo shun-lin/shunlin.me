4b7f2651f2741243a72bc36451f5e1d0b55eb306
Shun Lin <s92014721@berkeley.edu>
2018-07-27 22:49:46 -0700

    updated website to show update coming soon!
    updated website

--- js/intro_massage.js
+++ js/intro_massage.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     // updating website message
-    Materialize.toast('Welcome to my new personal website!', 2000);
+    Materialize.toast('New Update Coming Soon!', 2000);
 });
